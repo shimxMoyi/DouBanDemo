8e8840f7b04cfcf70ae1c4ec06f33bb12f771f7e
shimingxin <shimingxing@ysbang.cn>
2021-01-21 10:11:58 +0800

    initialize

---
+++ lib/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+// ignore: camel_case_types
+class home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      
+    );
+  }
+  
+}
