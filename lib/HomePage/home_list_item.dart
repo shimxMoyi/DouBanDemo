e8d0f93f8a463c4d7662c09946b7bd748756a10c
shimingxin <shimingxing@ysbang.cn>
2021-01-22 18:04:10 +0800

    home item ui

--- lib/HomePage/home_list_item.dart
+++ lib/HomePage/home_list_item.dart
@@ -12,11 +12,29 @@ class MXHomeListItem extends StatelessWidget {
 
     return Container(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
+          coverBuilder(),
           Text(movie.title)
         ],
       ),
     );
   }
+
+Widget titleBuilder() {
+  return Container(
+    child: Text(movie.title, style: TextStyle(fontSize: 20.0, color: Colors.grey, fontWeight: FontWeight.bold),)
+  );
+}
+
+Widget coverBuilder() {
+  return ClipRRect(
+    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+    child: Image.network(
+      movie.cover,
+      height: 120,
+      width: 80,),
+  );
+}
 }
 
